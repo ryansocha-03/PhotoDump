c02f1068d162fe60e8b27b37e4406a2e7ef49b39
Ryan <ryansocha03@gmail.com>
2025-11-09 21:26:51 -0500

    added events internal controller

---
+++ server/Internal.Api/Controllers/EventsController.cs
@@ -0,0 +1,37 @@
+using Infrastructure.EntityFramework.Models;
+using Infrastructure.EntityFramework.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Internal.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EventsController(IRepository<Event> repository): ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Event>>> GetAllEvents()
+    {
+        return Ok(await repository.GetAllAsync());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Event>> GetEventById(int id)
+    {
+        var eventResult = await repository.GetAsync(id);
+        if (eventResult is null)
+            return NotFound($"Event with id: {id} not found");
+        
+        return eventResult;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Event>> DeleteEvent(int id)
+    {
+        var eventResult = await repository.GetAsync(id);
+        if (eventResult is null)
+            return NotFound($"Event with id: {id} not found");
+        
+        await repository.DeleteAsync(id);
+        return eventResult;
+    }
+}
